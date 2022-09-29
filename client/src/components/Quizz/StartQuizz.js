9c67ce84520f272117f24abe2cc3e68ccbb6bcd6
Thebas57 <theohelf@gmail.com>
2022-09-29 12:39:15 +0200

    design quizz

--- client/src/components/Quizz/StartQuizz.js
+++ client/src/components/Quizz/StartQuizz.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getQuizz } from "../../actions/quizz.actions";
@@ -6,6 +5,7 @@ import { isEmpty, shuffleArray } from "../Utils";
 
 const StartQuizz = () => {
   const [loadQuizz, setLoadQuizz] = useState(true);
+  const [isLoading, seIsLoading] = useState(false);
   const [score, setScore] = useState(0);
 
   const quizz = useSelector((state) => state.quizzReducer);
@@ -14,16 +14,18 @@ const StartQuizz = () => {
   const dispatch = useDispatch();
 
   // Permets d'initialiser un nouveau mot pour le quizz
-  const initWord = () => {
+  const initWord = async () => {
     // On mélange le tableau + on choppe un id
-    const arrWords = shuffleArray(wordsData);
-    const idWord = arrWords[0]._id;
-    //On dispatch avec le nouvelle id
-    dispatch(getQuizz(idWord));
+      const arrWords = await shuffleArray(wordsData);
+      const idWord = arrWords[0]._id;
+      //On dispatch avec le nouvelle id
+      dispatch(getQuizz(idWord));
   };
 
   // Incrémente ou pas le score + appelle initScore
   const handleScore = () => {
+    // On remet le chrono à 0
+    const progressBar = document.getElementById("progress_bar");
     initWord();
     setScore(score + 1);
   };
@@ -31,7 +33,6 @@ const StartQuizz = () => {
   useEffect(() => {
     if (loadQuizz) {
       initWord();
-
       setLoadQuizz(false);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -39,14 +40,31 @@ const StartQuizz = () => {
 
   return (
     <>
-      <ul>
-        {!isEmpty(quizz.noTranslate) &&
-          quizz.noTranslate.map((question) => {
-            return <div key={question}>{question}</div>;
-          })}
-      </ul>
-      <button onClick={handleScore}>clique</button>
-      <h3>{score}</h3>
+      {!isEmpty(quizz) ? (
+        <>
+          <div className="question-quizz">
+            Traduction de : <span>{quizz.fr}</span>
+          </div>
+          <div id="progress_bar" className="progress_bar"></div>
+          <div className="quizz">
+            {!isEmpty(quizz.noTranslate) &&
+              quizz.noTranslate.map((question) => {
+                return (
+                  <div
+                    onClick={handleScore}
+                    className="response"
+                    key={question}
+                  >
+                    {question}
+                  </div>
+                );
+              })}
+          </div>
+          <h3>{score}</h3>
+        </>
+      ) : (
+        <i className="fas fa-spinner fa-spin"></i>
+      )}
     </>
   );
 };
