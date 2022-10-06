9849e667517f6195464f794bf979a15304df55f2
Thebas57 <theohelf@gmail.com>
2022-10-06 12:31:38 +0200

    Ajout de style + qques correctifs

--- client/src/components/Quizz/StartQuizz.js
+++ client/src/components/Quizz/StartQuizz.js
@@ -16,18 +16,41 @@ const StartQuizz = () => {
   // Permets d'initialiser un nouveau mot pour le quizz
   const initWord = async () => {
     // On mélange le tableau + on choppe un id
-      const arrWords = await shuffleArray(wordsData);
-      const idWord = arrWords[0]._id;
-      //On dispatch avec le nouvelle id
-      dispatch(getQuizz(idWord));
+    const arrWords = await shuffleArray(wordsData);
+    const idWord = arrWords[0]._id;
+    //On dispatch avec le nouvelle id
+    dispatch(getQuizz(idWord));
+  };
+
+  //Fonction qui reset l'animation du chrono + enlève les backgrounds des divs des anciennes réponses
+  const resetAnimation = () => {
+    document.getAnimations().forEach((anim) => {
+      anim.cancel();
+      anim.play();
+    });
+    //On enlève les class des divs repsonses
+    const divs = document.getElementsByClassName("response");
+    for (const element of divs) {
+      element.classList.remove("success-rep");
+      element.classList.remove("error-rep");
+    }
   };
 
   // Incrémente ou pas le score + appelle initScore
-  const handleScore = () => {
-    // On remet le chrono à 0
-    const progressBar = document.getElementById("progress_bar");
-    initWord();
-    setScore(score + 1);
+  const handleScore = (reponse) => {
+    //Si c'est la bonne réponse, on incrémente le score
+    if (quizz.translate === reponse.target.innerHTML) {
+      reponse.target.className += " success-rep";
+      setScore(score + 1);
+    } else {
+      //Mauvaise réponse
+      reponse.target.className += " error-rep";
+    }
+    setTimeout(() => {
+      // On remet le chrono à 0
+      resetAnimation();
+      initWord();
+    }, "1000");
   };
 
   useEffect(() => {
@@ -43,7 +66,10 @@ const StartQuizz = () => {
       {!isEmpty(quizz) ? (
         <>
           <div className="question-quizz">
-            Traduction de : <span>{quizz.fr}</span>
+            <div>
+              Traduction de : <span>{quizz.fr}</span>
+            </div>
+            <div>Score : {score}</div>
           </div>
           <div id="progress_bar" className="progress_bar"></div>
           <div className="quizz">
@@ -51,7 +77,7 @@ const StartQuizz = () => {
               quizz.noTranslate.map((question) => {
                 return (
                   <div
-                    onClick={handleScore}
+                    onClick={(question) => handleScore(question)}
                     className="response"
                     key={question}
                   >
@@ -60,7 +86,6 @@ const StartQuizz = () => {
                 );
               })}
           </div>
-          <h3>{score}</h3>
         </>
       ) : (
         <i className="fas fa-spinner fa-spin"></i>
--- client/src/pages/Accueil.js
+++ client/src/pages/Accueil.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Accueil = () => {
   return (
-    <div>Accueil</div>
-  )
-}
+    <div className="accueil">
+      <div className="div1">Bienvenue sur TRANSLATE QUIZZ</div>
+      <div className="div2">CC</div>
+      <div className="div3">CC</div>
+    </div>
+  );
+};
 
-export default Accueil
+export default Accueil;
